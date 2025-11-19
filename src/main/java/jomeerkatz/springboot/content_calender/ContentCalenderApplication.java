2b548bab74a31258e3bb1ca5ba730039f890d50f
jomeerkatz <jokirillmeerkatz@outlook.de>
2025-11-19 12:15:28 +0100

    list all beans

--- src/main/java/jomeerkatz/springboot/content_calender/ContentCalenderApplication.java
+++ src/main/java/jomeerkatz/springboot/content_calender/ContentCalenderApplication.java
@@ -2,12 +2,16 @@ package jomeerkatz.springboot.content_calender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ContentCalenderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ContentCalenderApplication.class, args);
-	}
-
+	public static <List> void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(ContentCalenderApplication.class, args);
+        String[] beans = context.getBeanDefinitionNames();
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+    }
 }
