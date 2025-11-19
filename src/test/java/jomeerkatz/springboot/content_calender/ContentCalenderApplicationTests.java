d4f53d1dfc9a5f07abe8d292ebf599486b018ddc
jomeerkatz <jokirillmeerkatz@outlook.de>
2025-11-19 11:50:19 +0100

    initial commit

---
+++ src/test/java/jomeerkatz/springboot/content_calender/ContentCalenderApplicationTests.java
@@ -0,0 +1,13 @@
+package jomeerkatz.springboot.content_calender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentCalenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
