1fcc2cdee1c43b435c24dbd70397ccd0cf9cd20a
jomeerkatz <jokirillmeerkatz@outlook.de>
2025-11-21 10:12:42 +0100

    connect to h2 database and remove old in memory repository. updated pom and readme

--- src/main/java/jomeerkatz/springboot/content_calender/controller/ContentController.java
+++ src/main/java/jomeerkatz/springboot/content_calender/controller/ContentController.java
@@ -1,7 +1,8 @@
 package jomeerkatz.springboot.content_calender.controller;
 
+import jakarta.validation.Valid;
 import jomeerkatz.springboot.content_calender.model.Content;
-import jomeerkatz.springboot.content_calender.repository.ContentCollectionRepository;
+import jomeerkatz.springboot.content_calender.repository.ContentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,45 +14,45 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
-    private ContentCollectionRepository contentCollectionRepository;
+    private ContentRepository contentRepository;
 
     public ContentController() { }
 
     @Autowired
-    public ContentController(ContentCollectionRepository contentCollectionRepository) {
-        this.contentCollectionRepository = contentCollectionRepository;
+    public ContentController(ContentRepository contentRepository) {
+        this.contentRepository = contentRepository;
     }
 
     @GetMapping("")
     public List<Content> findAll() {
-        return contentCollectionRepository.findAll();
+        return contentRepository.findAll();
     }
 
     @GetMapping("/{id}")
     public Content findSpecificContentById(@PathVariable Integer id) {
-        return contentCollectionRepository.findSpecificContent(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content with the ID not found!"));
+        return contentRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content with the ID not found!"));
     }
 
     @ResponseStatus(HttpStatus.CREATED) // this an be optimized. example: check if it's really in the db or list
     @PutMapping("")
-    public void create(@RequestBody Content content){
-        contentCollectionRepository.save(content);
+    public void create(@Valid @RequestBody Content content){
+        contentRepository.save(content);
     }
 
     @PostMapping("/{id}")
-    public ResponseEntity<?> update(@RequestBody Content content, @PathVariable Integer id) {
+    public ResponseEntity<?> update(@Valid @RequestBody Content content, @PathVariable Integer id) {
         // check if the content with the id even exists
-        if (!contentCollectionRepository.existsById(id)) {
+        if (!contentRepository.existsById(id)) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Content with the ID not found!");
         } else {
-            contentCollectionRepository.save(content);
+            contentRepository.save(content);
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         }
     }
 
     @DeleteMapping("/{id}") // return entity can be optimized
     public ResponseEntity<?> delete(@PathVariable Integer id) {
-        contentCollectionRepository.delete(id);
+        contentRepository.deleteById(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
